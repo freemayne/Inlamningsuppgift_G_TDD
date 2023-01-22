41cb4e83af1056fd084d5c88cdb1866b50840f10
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2023-01-22 16:07:02 +0100

    changed wrong imports and some small changes and created 2nd red phase on if errors appear on authenticate

--- src/test/java/com/example/inlamningsuppgift_g_tdd/auth/AuthenticationTest.java
+++ src/test/java/com/example/inlamningsuppgift_g_tdd/auth/AuthenticationTest.java
@@ -3,14 +3,13 @@ package com.example.inlamningsuppgift_g_tdd.auth;
 import com.example.inlamningsuppgift_g_tdd.entities.AppUser;
 import com.example.inlamningsuppgift_g_tdd.repo.UserRepo;
 import com.example.inlamningsuppgift_g_tdd.service.UserService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
@@ -19,18 +18,22 @@ import static org.mockito.Mockito.when;
 
 
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class AuthenticationTest {
 
     @Mock
     private UserRepo userRepo;
 
-    @InjectMocks
     private UserService userService;
 
     private final static long id = 1;
     private final static String username = "test";
     private final static String password = "test123";
+
+    @BeforeEach
+    public void init(){
+        userService = new UserService(userRepo);
+    }
 
     @Test
     public void testLogin_ifWorking_returnTrue()  {
@@ -38,10 +41,26 @@ public class AuthenticationTest {
         AppUser appUser = new AppUser(id, username, password);
         when(userRepo.findUserByUsername("test")).thenReturn(Optional.of(appUser));
 
+        boolean result = userService.authenticate("test", "test123");
+
         //When, then
-        assertTrue(userService.authenticate("test", "test123"));
+        assertTrue(result);
 
     }
+    @ParameterizedTest
+    @CsvSource(value = {"test,test1234","testare,test123"})
+    public void login_throwErrorIfWrongUsernameOrPassword(String testUsername, String testPassword) {
+
+        //Given
+        AppUser appUser = new AppUser(id,username, password);
+        when(userRepo.findUserByUsername(appUser.getUsername())).thenReturn(Optional.of(appUser));
+
+        //When, then
+        assertThrows(IllegalArgumentException.class, () -> userService.authenticate(testUsername, testPassword));
+
+    }
+
+
 
 
 }
